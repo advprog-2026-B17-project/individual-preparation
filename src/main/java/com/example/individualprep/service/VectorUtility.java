f60c096c878e3baccbd8111cad756af23374b5e7
Raymundo7532 <woloblocrafael@gmail.com>
2026-02-12 17:21:48 +0700

    Initialization of vector-add method

--- src/main/java/com/example/individualprep/service/VectorUtility.java
+++ src/main/java/com/example/individualprep/service/VectorUtility.java
@@ -6,8 +6,21 @@ import org.springframework.stereotype.Service;
 public class VectorUtility {
     
     public double[] add(double[] v1, double[] v2) {
-        // TODO: Implement me properly!
-        return new double[] { 0.0, 0.0, 0.0 };
+        if (v1 == null || v2 == null) {
+            if (v1 == null && v2 != null)
+                throw new IllegalArgumentException("Vektor pertama tidak boleh null!");
+            if (v1 != null && v2 == null)
+                throw new IllegalArgumentException("Vektor kedua tidak boleh null!");
+            throw new IllegalArgumentException("Kedua vektor tidak boleh null!");
+        }
+        if (v1.length != v2.length) {
+            throw new IllegalArgumentException("Dimensi kedua vektor harus sama!");
+        }
+        double[] result = new double[v1.length];
+        for (int index = 0; index < v1.length; index++) {
+            result[index] = v1[index] + v2[index];
+        }
+        return result;
     }
 
     public double[] subtract(double[] v1, double[] v2) {
